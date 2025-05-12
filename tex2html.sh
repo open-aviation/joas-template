4dc8622633f0c17d5b307bd05865dbb50e944321
Junzi Sun <junzisun@gmail.com>
2025-05-12 17:01:30 +0200

    update html

--- tex2html.sh
+++ tex2html.sh
@@ -11,11 +11,16 @@ pandoc main.tex -t json | python extra/joas_filter.py | pandoc -f json  \
   --variable editat=main \
   --extract-media build/html
 
-# remove line break between embed and src
-sed -i ':a;N;$!ba;s/embed\n\s*/embed /g' build/html/main.html
+# remove line break between <embed and src using perl for cross-platform compatibility
+perl -0777 -pe 's/<embed\s*\n\s*/<embed /g' build/html/main.html > build/html/main.tmp && mv build/html/main.tmp build/html/main.html
+
 
 # replace embed pdf figure with png
-sed -i 's/embed.*\/\(.*\)pdf/img src="build\/html\/figures\/\1png/g' build/html/main.html;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' 's/embed.*\/\(.*\)pdf/img src="build\/html\/figures\/\1png/g' build/html/main.html;
+else
+  sed -i 's/embed.*\/\(.*\)pdf/img src="build\/html\/figures\/\1png/g' build/html/main.html;
+fi
 
 
 find build/html/figures -type f -name '*.pdf' -print0 |
@@ -43,21 +48,26 @@ find build/html/figures -type f -name '*.png' -print0 |
   done
 
 
-# Replace newline characters with space after "<img" and space before "style="
-sed -i -z 's/<img\n/<img /g' build/html/main.html
+# Replace newline characters with space after "<img" using perl for cross-platform compatibility
+perl -0777 -pe 's/<img\s*\n\s*/<img /g' build/html/main.html > build/html/main.tmp && mv build/html/main.tmp build/html/main.html
 
 # replace all image with base64 encoding
-for img_orign in $(sed -n 's/.*<img\s*src="\([^"]*\)".*/\1/p' build/html/main.html)
+# Process images in smaller batches to avoid argument list too long error
+for img_origin in $(grep -o '<img[^>]*src="[^"]*"' build/html/main.html | sed -n 's/.*src="\([^"]*\)".*/\1/p')
 do
-  img_png=$(echo $img_orign | sed 's/\.[^.]*$/.png/')
+  img_png=$(echo $img_origin | sed 's/\.[^.]*$/.png/')
   # Encode the image in base64
   echo $img_png
-  base64 -w 0 "$img_png" > tmp.b64
-  
-  echo "s|$img_orign|data:image/png;base64,$(cat tmp.b64)|g" > tmp.sed
-  # Run the sed script on the HTML file
-  sed -i -f tmp.sed build/html/main.html
-  
+  base64 "$img_png" | tr -d '\n' > tmp.b64
+
+  echo "s|$img_origin|data:image/png;base64,$(cat tmp.b64)|g" > tmp.sed
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -f tmp.sed build/html/main.html
+  else
+    sed -i -f tmp.sed build/html/main.html
+  fi
+
   rm tmp.sed
   rm tmp.b64
 done
